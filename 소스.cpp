16feb50ea1d66c1c901a914472496a3364e14969
leebrobro <ssgodda@naver.com>
2022-01-06 18:52:40 +0900

    zz

--- 소스.cpp
+++ 소스.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 char s[111];
 int main() {
-	while (scanf("%[^\n]\n", s) == 1) {               
+	while (scanf("%[^\n]\n", s) == 1) {               //\n까지 받고 \n은 뺴라
 		printf("%s\n", s);
 	}
 	return 0;
